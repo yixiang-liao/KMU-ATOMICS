22c00ee7896d28cdf8aeaadc5655f5e6a1a23ac0
廖翊翔 <82229244+yixiang-liao@users.noreply.github.com>
2025-09-26 13:15:46 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kmu-atomics/',            // 部署在根目錄就用預設 '/'
+  build: { outDir: 'dist' }
 })
